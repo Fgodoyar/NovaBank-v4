618aaeddcc2365e075a8570ed3c699cbe063fc46
TKNFGOAR <frangoar22@gmail.com>
2026-05-11 04:08:49 +0200

    Migración de SecurityConfig, OpenApi, LoginRequestDTO, LoginResponseDTO, JwtService, UserService y creación de User, UserRepository y AuthController

---
+++ auth-server/src/main/java/com/novabank/auth/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.novabank.auth.repository;
+
+import com.novabank.auth.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    boolean existsByUsername(String username);
+}
